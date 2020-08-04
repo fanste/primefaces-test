03af29d82e5005baf6189ffeb44b5cef0419caab
fanste <mails@ist-einmalig.de>
2020-08-04 17:48:24 +0200

    Implement reproducer for primefaces#6192

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,49 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+
 @Named
 @ViewScoped
 public class TestView implements Serializable {
+    private MenuModel model;
     
-    private String testString;
+    @PostConstruct
+    public void __init() {
+    	model = new DefaultMenuModel();
+
+        DefaultMenuItem item1 = DefaultMenuItem.builder()
+                .value("Item 1")
+                .command("#{testView.doItemAction}")
+                .build();
+
+        DefaultMenuItem item2 = DefaultMenuItem.builder()
+                .value("Item 2")
+                .command("#{testView.doItemAction}")
+                .build();
+ 
+
+        model.getElements().add(item1);
+        model.getElements().add(item2);
+    }
     
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+    public MenuModel getModel() {
+    	return model;
     }
-
-    public String getTestString() {
-        return testString;
-    }
-
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    
+	public void doButtonAction() {
+		System.out.println("ButtonAction");
+	}
+	
+	public void doItemAction() {
+		System.out.println("ItemAction");
+	}
 }
